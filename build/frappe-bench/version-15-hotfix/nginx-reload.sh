c653c99fcdb4e056e1c9a2048e8612d113340979
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-05-26 02:46:20 +0530

    fix: frappe bench issues

--- build/frappe-bench/version-15-hotfix/nginx-reload.sh
+++ build/frappe-bench/version-15-hotfix/nginx-reload.sh
@@ -2,4 +2,5 @@
 
 cd /home/frappe/frappe-bench && \
   bench setup nginx --yes && \
+  cp /home/frappe/frappe-bench/config/nginx.conf /etc/nginx/conf.d/frappe-bench.conf && \
   supervisorctl restart frappe-bench-web:frappe-nginx
