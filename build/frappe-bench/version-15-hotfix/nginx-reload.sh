9852dc1c21946c26a4f64a3688732b9043f21054
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-05-26 12:13:34 +0530

    revert: frappe bench config

--- build/frappe-bench/version-15-hotfix/nginx-reload.sh
+++ build/frappe-bench/version-15-hotfix/nginx-reload.sh
@@ -2,5 +2,4 @@
 
 cd /home/frappe/frappe-bench && \
   bench setup nginx --yes && \
-  cp /home/frappe/frappe-bench/config/nginx.conf /etc/nginx/conf.d/frappe-bench.conf && \
   supervisorctl restart frappe-bench-web:frappe-nginx
